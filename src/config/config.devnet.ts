c05020f40e910d4943c30e163e4bbd38e51be0ae
paradogs-7 <vnyz2002@gmail.com>
2025-03-02 15:50:34 +0300

    update Vite version and change contract address for devnet configuration

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -3,7 +3,7 @@ import { EnvironmentsEnum } from 'types';
 export * from './sharedConfig';
 
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqm6ad6xrsjvxlcdcffqe8w58trpec09ug9l5qde96pq';
+  'erd1qp0khrzn60cg53p2h9h86wz7ht76jzdy7l7kxtzsjw2pqst79hjs0f5end';
 export const API_URL = 'https://devnet-template-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
